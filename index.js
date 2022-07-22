c7ac13c2da89d9fb78fd75cb19151546748559e3
dangrunger <dangrunger@gmail.com>
2022-07-22 14:00:21 -0300

    ✨ Creating Server

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const sqlite3 = require('sqlite3')
+const session = require('express-session')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs')
+
+app.use(session({
+    secret: 'supersecret',
+}))
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+const db = new sqlite3.Database('db.sqlite')
+db.serialize(() => {
+    db.run('CREATE TABLE IF NOT EXISTS `users` (`user_id` INTEGER PRIMARY KEY AUTOINCREMENT, `email` VARCHAR(255) NOT NULL, `secret` varchar(255) NOT NULL)')
+})
+db.close()
+
+app.listen(port, () => {
+    console.log(`2FA Node app listening at http://localhost:${port}`)
+})
